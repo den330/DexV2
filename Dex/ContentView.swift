af393ae5617b17a35304a38c6c70140ed68145ca
den330 <wo131485508@qq.com>
2025-03-04 13:07:06 -0500

    L7

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
+    
+    let fetcher = FetchService()
 
     var body: some View {
         NavigationView {
@@ -33,8 +35,33 @@ struct ContentView: View {
                 }
                 ToolbarItem {
                     Button("Add Item", systemImage: "plus") {
-                        
+                        getPokemon()
                     }
+                }
+            }
+        }
+    }
+    
+    private func getPokemon() {
+        Task {
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    let pokemon = Pokemon(context: viewContext)
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.shiny = fetchedPokemon.shiny
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefense = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.types = fetchedPokemon.types
+                    try viewContext.save()
+                } catch {
+                    print(error)
                 }
             }
         }
